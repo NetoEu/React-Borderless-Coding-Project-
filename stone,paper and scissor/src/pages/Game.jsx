652bb7b6494c9a0af9396592c2d3fd7f21818251
Neto <netobjsc@gmail.com>
2024-01-02 20:39:34 -0300

    add player2 image and score on Result page

--- stone,paper and scissor/src/pages/Game.jsx
+++ stone,paper and scissor/src/pages/Game.jsx
@@ -8,23 +8,47 @@ import { MyhookContext } from "../context/myfirsthook";
 
 const Game = () => {
 
-    const {imgResult, playerWin, player1} = useContext(MyhookContext);
-
+    const {imgResult, playerWin, player1, score} = useContext(MyhookContext);
 
     return (
         <>
-            <div>
-                <Link to="/">Play Again</Link>
+        <div>
+            Result
+        </div>
+        <div className='App'>
+            <div className='div1'>
+                <div className='frontScore'>
+                    <div className='player1Score'>
+                        <p className='numberPlayer1Score'>
+                            {score}
+                        </p>
+                    </div>
+                    <div className='gameName'>
+                        <p>
+                        Pedra
+                        </p>
+                        <p>
+                        Papel
+                        </p>
+                        <p>
+                        Tesoura
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <Link to="/">Play Again</Link>
+                </div>
+                <p>
+                    O Resultado é: {playerWin}
+                </p>
+                <div className="imgResultPlayer1">
+                    {imgResult(player1)}
+                </div>
+                <div className="imgResultPlayer2">
+                    {imgResult(player1)}
+                </div>
             </div>
-            <p>
-                O Resultado é: {playerWin}
-            </p>
-            <br />
-            <div>
-                {imgResult(player1)}
-            </div>
-            <div>
-            </div>
+        </div>
         </>
     );
 }
